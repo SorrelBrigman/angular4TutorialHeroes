e0819ab38e41f5e5fd22e70c2eba323eccfa861a
Sorrel Brigman <sorrel.k@gmail.com>
2018-04-18 19:13:46 -0500

    completed displaying a list portion of the tutorial

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Captain America' },
+  { id: 12, name: 'Black Widow' },
+  { id: 13, name: 'Iron Man' },
+  { id: 14, name: 'Ant Man' },
+  { id: 15, name: 'Star Lord' },
+  { id: 16, name: 'Vision' },
+  { id: 17, name: 'War Machine' },
+  { id: 18, name: 'Black Panthor' },
+  { id: 19, name: 'Hawk Eye' },
+  { id: 20, name: 'Agent Coleson' }
+];
